9a479d127bab7262cbb6826ae68f31e04c787ef2
Alex Rodriguez <rodriguez.al183@gmail.com>
2024-09-11 15:49:03 -0700

    Reformat tabs/spaces; added sqlite3_bind_text calls for createCard function; add getNextCardTime function

--- database/db.cpp
+++ database/db.cpp
@@ -14,8 +14,8 @@ bool openDB(sqlite3 **db, const char *dbFile)
 {
     int rc = sqlite3_open(dbFile, db);
     if (rc) {
-	std::cerr << "Can't open database " << sqlite3_errmsg(*db) << std::endl;
-	return false;
+        std::cerr << "Can't open database " << sqlite3_errmsg(*db) << std::endl;
+        return false;
     }
 
     std::cout << "Opened database " << dbFile << std::endl;
@@ -44,9 +44,9 @@ bool createDBTables(sqlite3 *db, char **errMsg)
     int rc = sqlite3_exec(db, createTableQuery, callback, 0, errMsg);    
 
     if (rc != SQLITE_OK) {
-	std::cerr << "SQL Error: " << *errMsg << std::endl;
-	sqlite3_free(*errMsg);
-	return false;
+        std::cerr << "SQL Error: " << *errMsg << std::endl;
+        sqlite3_free(*errMsg);
+        return false;
     }
 
     std::cout << "Card table created or already exists." << std::endl;
@@ -65,15 +65,15 @@ bool pullCard(sqlite3 *db, FlashCard **flash_card) {
 
     const char *pullQuery = R"(
     	SELECT * FROM cards
-	WHERE julianday(due) <= julianday(?)
-	ORDER BY ABS(julianday(due) - julianday(?))
-	ASC LIMIT 1;
+        WHERE julianday(due) <= julianday(?)
+        ORDER BY ABS(julianday(due) - julianday(?))
+        ASC LIMIT 1;
     )";
 
     sqlite3_stmt *stmt;
     if (sqlite3_prepare_v2(db, pullQuery, -1, &stmt, nullptr) != SQLITE_OK) {
-	std::cerr << "Failed to prepare sql: " << sqlite3_errmsg(db) << std::endl;
-	return false;;
+        std::cerr << "Failed to prepare sql: " << sqlite3_errmsg(db) << std::endl;
+        return false;;
     }
 
     sqlite3_bind_text(stmt, 1, curr_time_str.c_str(), -1, SQLITE_STATIC);
@@ -85,29 +85,29 @@ bool pullCard(sqlite3 *db, FlashCard **flash_card) {
     std::string answer_string = "";
 
     if (sqlite3_step(stmt) == SQLITE_ROW) {
-	uuid = sqlite3_column_int(stmt, 0);
-	card_map["due"] = reinterpret_cast<const char*>(sqlite3_column_text(stmt, 1));
-	card_map["stability"] = reinterpret_cast<const char*>(sqlite3_column_text(stmt, 2));
-	card_map["difficulty"] = reinterpret_cast<const char*>(sqlite3_column_text(stmt, 3));
-	card_map["elapsedDays"] = reinterpret_cast<const char*>(sqlite3_column_text(stmt, 4));
-	card_map["scheduledDays"] = reinterpret_cast<const char*>(sqlite3_column_text(stmt, 5));
-	card_map["reps"] = reinterpret_cast<const char*>(sqlite3_column_text(stmt, 6));
-	card_map["lapses"] = reinterpret_cast<const char*>(sqlite3_column_text(stmt, 7));
-	card_map["state"] = reinterpret_cast<const char*>(sqlite3_column_text(stmt, 8));
+        uuid = sqlite3_column_int(stmt, 0);
+        card_map["due"] = reinterpret_cast<const char*>(sqlite3_column_text(stmt, 1));
+        card_map["stability"] = reinterpret_cast<const char*>(sqlite3_column_text(stmt, 2));
+        card_map["difficulty"] = reinterpret_cast<const char*>(sqlite3_column_text(stmt, 3));
+        card_map["elapsedDays"] = reinterpret_cast<const char*>(sqlite3_column_text(stmt, 4));
+        card_map["scheduledDays"] = reinterpret_cast<const char*>(sqlite3_column_text(stmt, 5));
+        card_map["reps"] = reinterpret_cast<const char*>(sqlite3_column_text(stmt, 6));
+        card_map["lapses"] = reinterpret_cast<const char*>(sqlite3_column_text(stmt, 7));
+        card_map["state"] = reinterpret_cast<const char*>(sqlite3_column_text(stmt, 8));
 
-	const char* lr = reinterpret_cast<const char*>(sqlite3_column_text(stmt, 9));
+        const char* lr = reinterpret_cast<const char*>(sqlite3_column_text(stmt, 9));
 
-	if (strcmp(lr,"NULL") != 0)
-	    card_map["lastReview"] = lr;
+        if (strcmp(lr,"NULL") != 0)
+            card_map["lastReview"] = lr;
 
-	question_string = reinterpret_cast<const char*>(sqlite3_column_text(stmt, 10));
-	answer_string = reinterpret_cast<const char*>(sqlite3_column_text(stmt, 11));
-	
-	Card card = Card::fromMap(card_map);
+        question_string = reinterpret_cast<const char*>(sqlite3_column_text(stmt, 10));
+        answer_string = reinterpret_cast<const char*>(sqlite3_column_text(stmt, 11));
+        
+        Card card = Card::fromMap(card_map);
 
-	*flash_card = allocFlashCard(uuid, card, answer_string, question_string);
+        *flash_card = allocFlashCard(uuid, card, answer_string, question_string);
     } else {
-	std::cout << "There is no valid card to pull at this time" << std::endl;
+        std::cout << "There is no valid card to pull at this time" << std::endl;
     }
 
     sqlite3_finalize(stmt);
@@ -121,30 +121,39 @@ bool createCard(sqlite3 *db, char **errMsg, char* const question, char* const an
 
     std::unordered_map<std::string, std::string> card_map = new_card.toMap();
 
-    std::string insertSQL = ( 
-	    "INSERT INTO cards"
-	    "(due, stability, difficulty, elapsedDays, scheduledDays, "
-	    "reps, lapses, state, lastReview, question, answer) "
-	    "VALUES ('"
-	    + card_map["due"]           + "', '"
-	    + card_map["stability"]     + "', '"
-	    + card_map["difficulty"]    + "', '"
-	    + card_map["elapsedDays"]   + "', '"
-	    + card_map["scheduledDays"] + "', '"
-	    + card_map["reps"] 	        + "', '"
-	    + card_map["lapses"]        + "', '"
-	    + card_map["state"]         + "', '"
-	    + "NULL" 		        + "', '"
-	    + question                  + "', '"
-	    + answer 	     	        + "');"
-    );
+    const char *insertQuery = R"(
+        INSERT INTO cards
+        (due, stability, difficulty, elapsedDays, scheduledDays,
+        reps, lapses, state, lastReview, question, answer)
+        VALUES ( ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?);
+    )";
     
-    int rc = sqlite3_exec(db, insertSQL.c_str(), nullptr, nullptr, errMsg);
-    if (rc != SQLITE_OK) {
-	std::cerr << "Error inserting into database: " << *errMsg << std::endl;
-	sqlite3_free(*errMsg);
-	return false;
-    };
+    sqlite3_stmt *stmt;
+
+    if (sqlite3_prepare_v2(db, insertQuery, -1, &stmt, nullptr) != SQLITE_OK) {
+        std::cerr << "Failed to prepare update statement: " << sqlite3_errmsg(db) << std::endl;
+        return false;
+    }
+    sqlite3_bind_text(stmt, 1, card_map["due"].c_str(), -1, SQLITE_STATIC);
+    sqlite3_bind_text(stmt, 2, card_map["stability"].c_str(), -1, SQLITE_STATIC);
+    sqlite3_bind_text(stmt, 3, card_map["difficulty"].c_str(), -1, SQLITE_STATIC);
+    sqlite3_bind_text(stmt, 4, card_map["elapsedDays"].c_str(), -1, SQLITE_STATIC);
+    sqlite3_bind_text(stmt, 5, card_map["scheduledDays"].c_str(), -1, SQLITE_STATIC);
+    sqlite3_bind_text(stmt, 6, card_map["reps"].c_str(), -1, SQLITE_STATIC);
+    sqlite3_bind_text(stmt, 7, card_map["lapses"].c_str(), -1, SQLITE_STATIC);
+    sqlite3_bind_text(stmt, 8, card_map["state"].c_str(), -1, SQLITE_STATIC);
+    sqlite3_bind_text(stmt, 9, "NULL", -1, SQLITE_STATIC);
+    sqlite3_bind_text(stmt, 10, question, -1, SQLITE_STATIC);
+    sqlite3_bind_text(stmt, 11, answer, -1, SQLITE_STATIC);
+
+    int rc = sqlite3_step(stmt);
+    if (rc != SQLITE_DONE) {
+       std::cerr << "Failed to create card: " << sqlite3_errmsg(db) << std::endl;
+       sqlite3_finalize(stmt);
+       return false;
+    }
+
+    sqlite3_finalize(stmt);
 
     return true;
 
@@ -154,7 +163,7 @@ bool updateCard(sqlite3 *db, const Card& new_card, const FlashCard *flash_card)
 {
     std::unordered_map<std::string, std::string> card_map = new_card.toMap();
 
-    const char *update = R"(
+    const char *updateQuery = R"(
 	UPDATE cards
 	SET due = ?, stability = ?, difficulty = ?, elapsedDays = ?,
 	    scheduledDays = ?, reps = ?, lapses = ?, state = ?,
@@ -164,9 +173,9 @@ bool updateCard(sqlite3 *db, const Card& new_card, const FlashCard *flash_card)
 
     sqlite3_stmt *stmt;
 
-    if (sqlite3_prepare_v2(db, update, -1, &stmt, nullptr) != SQLITE_OK) {
-	std::cerr << "Failed to prepare update statement: " << sqlite3_errmsg(db) << std::endl;
-	return false;
+    if (sqlite3_prepare_v2(db, updateQuery, -1, &stmt, nullptr) != SQLITE_OK) {
+        std::cerr << "Failed to prepare update statement: " << sqlite3_errmsg(db) << std::endl;
+        return false;
     }
 
     sqlite3_bind_text(stmt, 1, card_map["due"].c_str(), -1, SQLITE_STATIC);
@@ -189,6 +198,26 @@ bool updateCard(sqlite3 *db, const Card& new_card, const FlashCard *flash_card)
        std::cerr << "Failed to update row: " << sqlite3_errmsg(db) << std::endl;
        sqlite3_finalize(stmt);
        return false;
+    }
+
+    sqlite3_finalize(stmt);
+
+    return true;
+}
+
+bool getNextCardTime(sqlite3 *db, std::string& time_str) 
+{
+    time_str = "";
+    const char *nextCardTimeQuery = R"(SELECT due FROM cards ORDER BY due ASC LIMIT 1;)";
+
+    sqlite3_stmt *stmt;
+    if (sqlite3_prepare_v2(db, nextCardTimeQuery, -1, &stmt, nullptr) != SQLITE_OK) {
+        std::cerr << "Failed to prepare sql: " << sqlite3_errmsg(db) << std::endl;
+        return false;;
+    }
+
+    if (sqlite3_step(stmt) == SQLITE_ROW) {
+        time_str = reinterpret_cast<const char*>(sqlite3_column_text(stmt, 0));
     }
 
     sqlite3_finalize(stmt);
--- database/db.hpp
+++ database/db.hpp
@@ -15,6 +15,7 @@ bool createDBTables(sqlite3 *db, char **errMsg);
 bool pullCard(sqlite3 *db, FlashCard **flash_card);
 bool createCard(sqlite3 *db, char **errMsg, char* const question, char* const answer);
 bool updateCard(sqlite3 *db, const Card& new_card, const FlashCard *flash_card);
+bool getNextCardTime(sqlite3 *db, std::string& time_str);
 
 void closeDB(sqlite3 *db);
 #endif
